092f86483274f7d673060df42f0bf055b6c7cea2
illagrenan <vaclav.dohnal@gmail.com>
2013-08-09 17:08:53 +0200

    Doc code formatting

--- examples/configure-and-load-extension/bootstrap.php
+++ examples/configure-and-load-extension/bootstrap.php
@@ -1,16 +1,15 @@
 <?php
+
 // Load Nette Framework
 require LIBS_DIR . '/autoload.php';
 
 // ...
-
 // Configure application
 $configurator = new Nette\Config\Configurator;
 
 \Illagrenan\Facebook\DI\FacebookConnectExtension::register($configurator);
 
 // ...
-
 // Configure and run the application!
 $container->application->run();
 
